14ed32943aef1dcc750a7925592e51cf251a9317
William Clayton <wbc15@my.fsu.edu>
2018-12-08 20:26:56 +0000

    lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=32d96021669ae23e46daed547c57ebc7',
                 data: query
             })
             
@@ -56,7 +56,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#overview"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/                
+                 *********************************Insert Code Below********************************************/
+                 $("#overview").html(json.results[0].overview);
                 
                 
                 
@@ -71,7 +72,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#release_date"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/               
+                 *********************************Insert Code Below********************************************/
+                 $("#release_date").html(json.results[0].release_date);
 
 
 
@@ -86,7 +88,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#popularity"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/   
+                 *********************************Insert Code Below********************************************/
+                 $("#popularity").html(json.results[0].popularity);
 
   
 
@@ -101,7 +104,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#original_language"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/                 
+                 *********************************Insert Code Below********************************************/
+                 $("#original_language").html(json.results[0].original_language);
 
 
 
--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(December 5, 2018)
 
 ### Student Name:  
-(First and Last Name)
+(William Clayton)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(In the assignment we learn the basics and uses of AJAX and JSON while building a web page that uses an API from a movie website )
 
 ### View Project:
 (Replace this statement with your Github Page URL that was created when you 
  published the project.)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
-4. (Briefly describe a lesson/concept learned in this lesson.)
-5. (Briefly describe a lesson/concept learned in this lesson.)
+1. (We learned how to use an API.)
+2. (parse JSON into a response message.)
+3. (How to use jquery to make the webpage more user friendly.)
+4. (How to inspect a page to view its attributes and edit them based on the look of the page.)
+5. (How to use bootstrap to make the webpage look more put together.)
